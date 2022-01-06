4d39e29040524dfe1ea31f16c8ace0e3e650c85a
auterionci <ci@auterion.com>
2022-01-06 13:24:20 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/8903436727c470e0476b1a355ca25eeaad56ad5b

--- common/mavlink_msg_smart_battery_info.h
+++ common/mavlink_msg_smart_battery_info.h
@@ -346,7 +346,7 @@ static inline void mavlink_msg_smart_battery_info_send_struct(mavlink_channel_t 
 
 #if MAVLINK_MSG_ID_SMART_BATTERY_INFO_LEN <= MAVLINK_MAX_PAYLOAD_LEN
 /*
-  This varient of _send() can be used to save stack space by re-using
+  This variant of _send() can be used to save stack space by re-using
   memory from the receive buffer.  The caller provides a
   mavlink_message_t which is the size of a full mavlink message. This
   is usually the receive buffer for the channel, and allows a reply to an
