49bd7bffabc0bc459b1ebf602bf014481d3fb134
auterionci <ci@auterion.com>
2022-01-19 17:12:15 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/18f976865bf14bc01eafc2c50b08ddaccd87e8d3

--- development/mavlink_msg_component_information_basic.h
+++ development/mavlink_msg_component_information_basic.h
@@ -5,7 +5,7 @@
 
 
 typedef struct __mavlink_component_information_basic_t {
- uint64_t component_cap_flags1; /*<  Component capability flags 1 (this is called 1, so that number 2 could be added in the future).*/
+ uint64_t capabilities; /*<  Component capability flags*/
  uint32_t time_boot_ms; /*< [ms] Timestamp (time since system boot).*/
  uint8_t vendor_name[32]; /*<  Name of the component vendor*/
  uint8_t model_name[32]; /*<  Name of the component model*/
@@ -18,8 +18,8 @@ typedef struct __mavlink_component_information_basic_t {
 #define MAVLINK_MSG_ID_396_LEN 124
 #define MAVLINK_MSG_ID_396_MIN_LEN 124
 
-#define MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_CRC 45
-#define MAVLINK_MSG_ID_396_CRC 45
+#define MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_CRC 122
+#define MAVLINK_MSG_ID_396_CRC 122
 
 #define MAVLINK_MSG_COMPONENT_INFORMATION_BASIC_FIELD_VENDOR_NAME_LEN 32
 #define MAVLINK_MSG_COMPONENT_INFORMATION_BASIC_FIELD_MODEL_NAME_LEN 32
@@ -36,7 +36,7 @@ typedef struct __mavlink_component_information_basic_t {
          { "model_name", NULL, MAVLINK_TYPE_UINT8_T, 32, 44, offsetof(mavlink_component_information_basic_t, model_name) }, \
          { "software_version", NULL, MAVLINK_TYPE_CHAR, 24, 76, offsetof(mavlink_component_information_basic_t, software_version) }, \
          { "hardware_version", NULL, MAVLINK_TYPE_CHAR, 24, 100, offsetof(mavlink_component_information_basic_t, hardware_version) }, \
-         { "component_cap_flags1", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_component_information_basic_t, component_cap_flags1) }, \
+         { "capabilities", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_component_information_basic_t, capabilities) }, \
          } \
 }
 #else
@@ -48,7 +48,7 @@ typedef struct __mavlink_component_information_basic_t {
          { "model_name", NULL, MAVLINK_TYPE_UINT8_T, 32, 44, offsetof(mavlink_component_information_basic_t, model_name) }, \
          { "software_version", NULL, MAVLINK_TYPE_CHAR, 24, 76, offsetof(mavlink_component_information_basic_t, software_version) }, \
          { "hardware_version", NULL, MAVLINK_TYPE_CHAR, 24, 100, offsetof(mavlink_component_information_basic_t, hardware_version) }, \
-         { "component_cap_flags1", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_component_information_basic_t, component_cap_flags1) }, \
+         { "capabilities", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_component_information_basic_t, capabilities) }, \
          } \
 }
 #endif
@@ -64,15 +64,15 @@ typedef struct __mavlink_component_information_basic_t {
  * @param model_name  Name of the component model
  * @param software_version  Sofware version. The version format can be custom, recommended is SEMVER 'major.minor.patch'.
  * @param hardware_version  Hardware version. The version format can be custom, recommended is SEMVER 'major.minor.patch'.
- * @param component_cap_flags1  Component capability flags 1 (this is called 1, so that number 2 could be added in the future).
+ * @param capabilities  Component capability flags
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_component_information_basic_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                               uint32_t time_boot_ms, const uint8_t *vendor_name, const uint8_t *model_name, const char *software_version, const char *hardware_version, uint64_t component_cap_flags1)
+                               uint32_t time_boot_ms, const uint8_t *vendor_name, const uint8_t *model_name, const char *software_version, const char *hardware_version, uint64_t capabilities)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN];
-    _mav_put_uint64_t(buf, 0, component_cap_flags1);
+    _mav_put_uint64_t(buf, 0, capabilities);
     _mav_put_uint32_t(buf, 8, time_boot_ms);
     _mav_put_uint8_t_array(buf, 12, vendor_name, 32);
     _mav_put_uint8_t_array(buf, 44, model_name, 32);
@@ -81,7 +81,7 @@ static inline uint16_t mavlink_msg_component_information_basic_pack(uint8_t syst
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN);
 #else
     mavlink_component_information_basic_t packet;
-    packet.component_cap_flags1 = component_cap_flags1;
+    packet.capabilities = capabilities;
     packet.time_boot_ms = time_boot_ms;
     mav_array_memcpy(packet.vendor_name, vendor_name, sizeof(uint8_t)*32);
     mav_array_memcpy(packet.model_name, model_name, sizeof(uint8_t)*32);
@@ -105,16 +105,16 @@ static inline uint16_t mavlink_msg_component_information_basic_pack(uint8_t syst
  * @param model_name  Name of the component model
  * @param software_version  Sofware version. The version format can be custom, recommended is SEMVER 'major.minor.patch'.
  * @param hardware_version  Hardware version. The version format can be custom, recommended is SEMVER 'major.minor.patch'.
- * @param component_cap_flags1  Component capability flags 1 (this is called 1, so that number 2 could be added in the future).
+ * @param capabilities  Component capability flags
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_component_information_basic_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
                                mavlink_message_t* msg,
-                                   uint32_t time_boot_ms,const uint8_t *vendor_name,const uint8_t *model_name,const char *software_version,const char *hardware_version,uint64_t component_cap_flags1)
+                                   uint32_t time_boot_ms,const uint8_t *vendor_name,const uint8_t *model_name,const char *software_version,const char *hardware_version,uint64_t capabilities)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN];
-    _mav_put_uint64_t(buf, 0, component_cap_flags1);
+    _mav_put_uint64_t(buf, 0, capabilities);
     _mav_put_uint32_t(buf, 8, time_boot_ms);
     _mav_put_uint8_t_array(buf, 12, vendor_name, 32);
     _mav_put_uint8_t_array(buf, 44, model_name, 32);
@@ -123,7 +123,7 @@ static inline uint16_t mavlink_msg_component_information_basic_pack_chan(uint8_t
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN);
 #else
     mavlink_component_information_basic_t packet;
-    packet.component_cap_flags1 = component_cap_flags1;
+    packet.capabilities = capabilities;
     packet.time_boot_ms = time_boot_ms;
     mav_array_memcpy(packet.vendor_name, vendor_name, sizeof(uint8_t)*32);
     mav_array_memcpy(packet.model_name, model_name, sizeof(uint8_t)*32);
@@ -146,7 +146,7 @@ static inline uint16_t mavlink_msg_component_information_basic_pack_chan(uint8_t
  */
 static inline uint16_t mavlink_msg_component_information_basic_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_component_information_basic_t* component_information_basic)
 {
-    return mavlink_msg_component_information_basic_pack(system_id, component_id, msg, component_information_basic->time_boot_ms, component_information_basic->vendor_name, component_information_basic->model_name, component_information_basic->software_version, component_information_basic->hardware_version, component_information_basic->component_cap_flags1);
+    return mavlink_msg_component_information_basic_pack(system_id, component_id, msg, component_information_basic->time_boot_ms, component_information_basic->vendor_name, component_information_basic->model_name, component_information_basic->software_version, component_information_basic->hardware_version, component_information_basic->capabilities);
 }
 
 /**
@@ -160,7 +160,7 @@ static inline uint16_t mavlink_msg_component_information_basic_encode(uint8_t sy
  */
 static inline uint16_t mavlink_msg_component_information_basic_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_component_information_basic_t* component_information_basic)
 {
-    return mavlink_msg_component_information_basic_pack_chan(system_id, component_id, chan, msg, component_information_basic->time_boot_ms, component_information_basic->vendor_name, component_information_basic->model_name, component_information_basic->software_version, component_information_basic->hardware_version, component_information_basic->component_cap_flags1);
+    return mavlink_msg_component_information_basic_pack_chan(system_id, component_id, chan, msg, component_information_basic->time_boot_ms, component_information_basic->vendor_name, component_information_basic->model_name, component_information_basic->software_version, component_information_basic->hardware_version, component_information_basic->capabilities);
 }
 
 /**
@@ -172,15 +172,15 @@ static inline uint16_t mavlink_msg_component_information_basic_encode_chan(uint8
  * @param model_name  Name of the component model
  * @param software_version  Sofware version. The version format can be custom, recommended is SEMVER 'major.minor.patch'.
  * @param hardware_version  Hardware version. The version format can be custom, recommended is SEMVER 'major.minor.patch'.
- * @param component_cap_flags1  Component capability flags 1 (this is called 1, so that number 2 could be added in the future).
+ * @param capabilities  Component capability flags
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_component_information_basic_send(mavlink_channel_t chan, uint32_t time_boot_ms, const uint8_t *vendor_name, const uint8_t *model_name, const char *software_version, const char *hardware_version, uint64_t component_cap_flags1)
+static inline void mavlink_msg_component_information_basic_send(mavlink_channel_t chan, uint32_t time_boot_ms, const uint8_t *vendor_name, const uint8_t *model_name, const char *software_version, const char *hardware_version, uint64_t capabilities)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN];
-    _mav_put_uint64_t(buf, 0, component_cap_flags1);
+    _mav_put_uint64_t(buf, 0, capabilities);
     _mav_put_uint32_t(buf, 8, time_boot_ms);
     _mav_put_uint8_t_array(buf, 12, vendor_name, 32);
     _mav_put_uint8_t_array(buf, 44, model_name, 32);
@@ -189,7 +189,7 @@ static inline void mavlink_msg_component_information_basic_send(mavlink_channel_
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC, buf, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_MIN_LEN, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_CRC);
 #else
     mavlink_component_information_basic_t packet;
-    packet.component_cap_flags1 = component_cap_flags1;
+    packet.capabilities = capabilities;
     packet.time_boot_ms = time_boot_ms;
     mav_array_memcpy(packet.vendor_name, vendor_name, sizeof(uint8_t)*32);
     mav_array_memcpy(packet.model_name, model_name, sizeof(uint8_t)*32);
@@ -207,7 +207,7 @@ static inline void mavlink_msg_component_information_basic_send(mavlink_channel_
 static inline void mavlink_msg_component_information_basic_send_struct(mavlink_channel_t chan, const mavlink_component_information_basic_t* component_information_basic)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_component_information_basic_send(chan, component_information_basic->time_boot_ms, component_information_basic->vendor_name, component_information_basic->model_name, component_information_basic->software_version, component_information_basic->hardware_version, component_information_basic->component_cap_flags1);
+    mavlink_msg_component_information_basic_send(chan, component_information_basic->time_boot_ms, component_information_basic->vendor_name, component_information_basic->model_name, component_information_basic->software_version, component_information_basic->hardware_version, component_information_basic->capabilities);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC, (const char *)component_information_basic, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_MIN_LEN, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_CRC);
 #endif
@@ -221,11 +221,11 @@ static inline void mavlink_msg_component_information_basic_send_struct(mavlink_c
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_component_information_basic_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint32_t time_boot_ms, const uint8_t *vendor_name, const uint8_t *model_name, const char *software_version, const char *hardware_version, uint64_t component_cap_flags1)
+static inline void mavlink_msg_component_information_basic_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint32_t time_boot_ms, const uint8_t *vendor_name, const uint8_t *model_name, const char *software_version, const char *hardware_version, uint64_t capabilities)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
-    _mav_put_uint64_t(buf, 0, component_cap_flags1);
+    _mav_put_uint64_t(buf, 0, capabilities);
     _mav_put_uint32_t(buf, 8, time_boot_ms);
     _mav_put_uint8_t_array(buf, 12, vendor_name, 32);
     _mav_put_uint8_t_array(buf, 44, model_name, 32);
@@ -234,7 +234,7 @@ static inline void mavlink_msg_component_information_basic_send_buf(mavlink_mess
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC, buf, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_MIN_LEN, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_LEN, MAVLINK_MSG_ID_COMPONENT_INFORMATION_BASIC_CRC);
 #else
     mavlink_component_information_basic_t *packet = (mavlink_component_information_basic_t *)msgbuf;
-    packet->component_cap_flags1 = component_cap_flags1;
+    packet->capabilities = capabilities;
     packet->time_boot_ms = time_boot_ms;
     mav_array_memcpy(packet->vendor_name, vendor_name, sizeof(uint8_t)*32);
     mav_array_memcpy(packet->model_name, model_name, sizeof(uint8_t)*32);
@@ -301,11 +301,11 @@ static inline uint16_t mavlink_msg_component_information_basic_get_hardware_vers
 }
 
 /**
- * @brief Get field component_cap_flags1 from component_information_basic message
+ * @brief Get field capabilities from component_information_basic message
  *
- * @return  Component capability flags 1 (this is called 1, so that number 2 could be added in the future).
+ * @return  Component capability flags
  */
-static inline uint64_t mavlink_msg_component_information_basic_get_component_cap_flags1(const mavlink_message_t* msg)
+static inline uint64_t mavlink_msg_component_information_basic_get_capabilities(const mavlink_message_t* msg)
 {
     return _MAV_RETURN_uint64_t(msg,  0);
 }
@@ -319,7 +319,7 @@ static inline uint64_t mavlink_msg_component_information_basic_get_component_cap
 static inline void mavlink_msg_component_information_basic_decode(const mavlink_message_t* msg, mavlink_component_information_basic_t* component_information_basic)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    component_information_basic->component_cap_flags1 = mavlink_msg_component_information_basic_get_component_cap_flags1(msg);
+    component_information_basic->capabilities = mavlink_msg_component_information_basic_get_capabilities(msg);
     component_information_basic->time_boot_ms = mavlink_msg_component_information_basic_get_time_boot_ms(msg);
     mavlink_msg_component_information_basic_get_vendor_name(msg, component_information_basic->vendor_name);
     mavlink_msg_component_information_basic_get_model_name(msg, component_information_basic->model_name);
