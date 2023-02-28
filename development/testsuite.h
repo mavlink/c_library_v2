a0d6e130be6c35f3c9a7ec181793c403871aefbf
auterionci <ci@auterion.com>
2023-02-28 16:55:58 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/06ca794fcb868335c4b03cf997f3d48ae1016dce

--- development/testsuite.h
+++ development/testsuite.h
@@ -275,6 +275,71 @@ static void mavlink_test_wifi_network_info(uint8_t system_id, uint8_t component_
 #endif
 }
 
+static void mavlink_test_cellular_modem_information(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_CELLULAR_MODEM_INFORMATION >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_cellular_modem_information_t packet_in = {
+        93372036854775807ULL,93372036854776311ULL,53,"RSTUVWXYZ","BCDEFGHIJKLMNOPQRST","VWXYZABCDEFGHIJKLMNOPQR","TUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOP"
+    };
+    mavlink_cellular_modem_information_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.imei = packet_in.imei;
+        packet1.imsi = packet_in.imsi;
+        packet1.id = packet_in.id;
+        
+        mav_array_memcpy(packet1.modem_id, packet_in.modem_id, sizeof(char)*10);
+        mav_array_memcpy(packet1.iccid, packet_in.iccid, sizeof(char)*20);
+        mav_array_memcpy(packet1.firmware, packet_in.firmware, sizeof(char)*24);
+        mav_array_memcpy(packet1.modem_model, packet_in.modem_model, sizeof(char)*50);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_CELLULAR_MODEM_INFORMATION_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_CELLULAR_MODEM_INFORMATION_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_cellular_modem_information_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_cellular_modem_information_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_cellular_modem_information_pack(system_id, component_id, &msg , packet1.id , packet1.imei , packet1.imsi , packet1.modem_id , packet1.iccid , packet1.firmware , packet1.modem_model );
+    mavlink_msg_cellular_modem_information_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_cellular_modem_information_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.id , packet1.imei , packet1.imsi , packet1.modem_id , packet1.iccid , packet1.firmware , packet1.modem_model );
+    mavlink_msg_cellular_modem_information_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_cellular_modem_information_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_cellular_modem_information_send(MAVLINK_COMM_1 , packet1.id , packet1.imei , packet1.imsi , packet1.modem_id , packet1.iccid , packet1.firmware , packet1.modem_model );
+    mavlink_msg_cellular_modem_information_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("CELLULAR_MODEM_INFORMATION") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_CELLULAR_MODEM_INFORMATION) != NULL);
+#endif
+}
+
 static void mavlink_test_figure_eight_execution_status(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -1086,6 +1151,7 @@ static void mavlink_test_development(uint8_t system_id, uint8_t component_id, ma
     mavlink_test_mission_checksum(system_id, component_id, last_msg);
     mavlink_test_airspeed(system_id, component_id, last_msg);
     mavlink_test_wifi_network_info(system_id, component_id, last_msg);
+    mavlink_test_cellular_modem_information(system_id, component_id, last_msg);
     mavlink_test_figure_eight_execution_status(system_id, component_id, last_msg);
     mavlink_test_battery_status_v2(system_id, component_id, last_msg);
     mavlink_test_component_information_basic(system_id, component_id, last_msg);
