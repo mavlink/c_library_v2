be52f853af3416154922ef50ec98a07a20308e9f
auterionci <ci@auterion.com>
2023-12-11 15:44:57 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/c98024bdf2b028cca69589b20b35a08d14ddba4a

--- development/development.h
+++ development/development.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_DEVELOPMENT.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_DEVELOPMENT_XML_HASH -7728369810358298028
+#define MAVLINK_DEVELOPMENT_XML_HASH 6357455748151470649
 
 #ifdef __cplusplus
 extern "C" {
--- ras_a/ras_a.h
+++ ras_a/ras_a.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_RAS_A.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_RAS_A_XML_HASH 8966410876496616564
+#define MAVLINK_RAS_A_XML_HASH 8907808164375934949
 
 #ifdef __cplusplus
 extern "C" {
