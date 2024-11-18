f08e800c2538a4584b87c80acfbac6bd514e0288
pakhomovda <pakhomovda@a-r-s.dev>
2024-11-18 13:00:11 +0300

    generate MAV_CMD_DO_SEARCH

--- standard/standard.h
+++ standard/standard.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_STANDARD.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_STANDARD_XML_HASH 7637019432294936274
+#define MAVLINK_STANDARD_XML_HASH -3342411019044502565
 
 #ifdef __cplusplus
 extern "C" {
--- ualberta/ualberta.h
+++ ualberta/ualberta.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UALBERTA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UALBERTA_XML_HASH -6332127600054784786
+#define MAVLINK_UALBERTA_XML_HASH -7377200291913676197
 
 #ifdef __cplusplus
 extern "C" {
