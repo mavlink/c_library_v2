a35bd432602ace30960a73d2f6d82c950cc5b971
redmonf <fergus@auterion.com>
2025-08-19 12:39:15 +0200

    added ship-landing

--- auterion/auterion.h
+++ auterion/auterion.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_AUTERION.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_AUTERION_XML_HASH 2597702218001468074
+#define MAVLINK_AUTERION_XML_HASH -2612402167553350544
 
 #ifdef __cplusplus
 extern "C" {
