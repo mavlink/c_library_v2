669b5a30a36fb0a84d0fadfd308d3e08098d5329
redmonf <fergus@auterion.com>
2025-09-08 11:20:48 +0200

    s

--- auterion/auterion.h
+++ auterion/auterion.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_AUTERION.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_AUTERION_XML_HASH 6547273555985145282
+#define MAVLINK_AUTERION_XML_HASH -5355200518231792310
 
 #ifdef __cplusplus
 extern "C" {
